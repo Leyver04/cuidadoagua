dd3756e8d748d52116e36a24177a3da35f5f2824
Leyver04 <leyverfabian@gmail.com>
2025-06-23 10:57:31 -0600

    Update app.js22

--- public/app.js
+++ public/app.js
@@ -216,7 +216,7 @@ function loadQuestion() {
   nextBtn.classList.add("hidden")
   submitBtn.classList.add("hidden")
   clearInterval(timer)
-  timeLeft = 10
+  timeLeft = 20
   updateTimerDisplay()
   timer = setInterval(updateTimer, 1000)
 
